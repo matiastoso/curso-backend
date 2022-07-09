1b78bc34f57679e34a4b46f4343ae7e0c018571c
Matías Toso <69355652+matiastoso@users.noreply.github.com>
2022-07-09 17:32:24 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -12,18 +12,32 @@ for (const prod of productos) {
     productosContenedor.save(prod)
 }
 
+const arrayDeProductos = productosContenedor.getAll();
+
 const server = app.listen(PORT, () => {
     console.log('Hola mundo');
 })
 
 app.get('/', (req, res) => {
-    res.send( {mensaje: 'hola desde el inicio'} )
+    res.send( `<h1>Hola desde el inicio</h1>` )
 })
 
 app.get('/productos', (req, res) => {
-    res.send(productosContenedor.getAll())
+    let html = `<h1> Listado de productos </h1>`;
+    for (const prod of arrayDeProductos) {
+        html += `<div>
+                    <img width="150px" height="150px" src="${prod.thumbnail}">
+                    <h3> ${prod.title} $${prod.price} </h3>
+                </div>`
+    }
+    res.send(html);
 })
 
 app.get('/productoRandom', (req, res) => {
-    res.send( {mensaje: 'productoRandom'} )
+    let numeroAzar = Math.trunc(Math.random() * 4);
+    let productoElegido = productosContenedor.getById(numeroAzar);
+    res.send( `<div>
+                    <img width="150px" height="150px" src="${productoElegido[0].thumbnail}">
+                    <h3> ${productoElegido[0].title} $${productoElegido[0].price} </h3>
+                </div>` );
 })
